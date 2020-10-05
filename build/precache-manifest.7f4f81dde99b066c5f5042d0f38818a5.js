4786a6a22ce3d920a83a436d9d2ea20f46e566a2
opyton <36433925+opyton@users.noreply.github.com>
2020-10-05 15:36:28 -0400

    HTTPS Request Update
    Title

---
+++ build/precache-manifest.7f4f81dde99b066c5f5042d0f38818a5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d520401e9c57114d05d5b3af44c4f41",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1aa900c6c509f28c091b",
+    "url": "/static/js/2.5f8d1605.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.5f8d1605.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a00a855098215384747a",
+    "url": "/static/js/main.c5b699b5.chunk.js"
+  },
+  {
+    "revision": "4a42fdcbdfd3cf55fea3",
+    "url": "/static/js/runtime-main.1c47a842.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.996fbe20550596f9cbaf5dc1a84e98d4.js"
+  "/precache-manifest.7f4f81dde99b066c5f5042d0f38818a5.js"
 );
 
 self.addEventListener('message', (event) => {
